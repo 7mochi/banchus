9f50b8bb14419bb89603e436dd83e326b2cfa66e
7mochi <flyingcatdm@gmail.com>
2026-02-10 15:08:11 -0500

    refactor: architectural overhaul of the packet abstraction

---
+++ src/main/java/pe/nanamochi/banchus/packets/handler/ReceiveUpdatesHandler.java
@@ -0,0 +1,28 @@
+package pe.nanamochi.banchus.packets.handler;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import pe.nanamochi.banchus.entities.db.Session;
+import pe.nanamochi.banchus.packets.client.ReceiveUpdatesPacket;
+import pe.nanamochi.banchus.packets.core.AbstractPacketHandler;
+import pe.nanamochi.banchus.packets.core.HandleClientPacket;
+import pe.nanamochi.banchus.packets.core.Packets;
+
+@Component
+@HandleClientPacket(Packets.OSU_RECEIVE_UPDATES)
+public class ReceiveUpdatesHandler extends AbstractPacketHandler<ReceiveUpdatesPacket> {
+  private static final Logger logger = LoggerFactory.getLogger(ReceiveUpdatesHandler.class);
+
+  @Override
+  public boolean checkForRestriction() {
+    return true;
+  }
+
+  @Override
+  public void handle(
+      ReceiveUpdatesPacket packet, Session session, ByteArrayOutputStream responseStream)
+      throws IOException {}
+}
---
+++ src/main/java/pe/nanamochi/banchus/packets/handler/UserStatsRequestHandler.java
@@ -0,0 +1,28 @@
+package pe.nanamochi.banchus.packets.handler;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import pe.nanamochi.banchus.entities.db.Session;
+import pe.nanamochi.banchus.packets.client.UserStatsRequestPacket;
+import pe.nanamochi.banchus.packets.core.AbstractPacketHandler;
+import pe.nanamochi.banchus.packets.core.HandleClientPacket;
+import pe.nanamochi.banchus.packets.core.Packets;
+
+@Component
+@HandleClientPacket(Packets.OSU_USER_STATS_REQUEST)
+public class UserStatsRequestHandler extends AbstractPacketHandler<UserStatsRequestPacket> {
+  private static final Logger logger = LoggerFactory.getLogger(UserStatsRequestHandler.class);
+
+  @Override
+  public boolean checkForRestriction() {
+    return true;
+  }
+
+  @Override
+  public void handle(
+      UserStatsRequestPacket packet, Session session, ByteArrayOutputStream responseStream)
+      throws IOException {}
+}
