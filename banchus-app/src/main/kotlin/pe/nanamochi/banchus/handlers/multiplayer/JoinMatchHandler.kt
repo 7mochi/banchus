8b0f342999fec608fc6e7462f6ed6e215bfd40b9
7mochi <flyingcatdm@gmail.com>
2026-04-27 17:09:52 -0500

    feat: send MatchJoinFailPacket when joining a password-protected match with an incorrect password

--- banchus-app/src/main/kotlin/pe/nanamochi/banchus/handlers/multiplayer/JoinMatchHandler.kt
+++ banchus-app/src/main/kotlin/pe/nanamochi/banchus/handlers/multiplayer/JoinMatchHandler.kt
@@ -6,14 +6,19 @@ import java.io.ByteArrayOutputStream
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import pe.nanamochi.banchus.core.PacketType
+import pe.nanamochi.banchus.domain.error.InvalidPassword
 import pe.nanamochi.banchus.infrastructure.protocol.AbstractPacketHandler
 import pe.nanamochi.banchus.infrastructure.protocol.HandleClientPacket
 import pe.nanamochi.banchus.packets.client.JoinMatchPacket
+import pe.nanamochi.banchus.packets.server.AnnouncePacket
 import pe.nanamochi.banchus.packets.server.ChannelJoinSuccessPacket
+import pe.nanamochi.banchus.packets.server.MatchJoinFailPacket
 import pe.nanamochi.banchus.packets.server.MatchJoinSuccessPacket
 import pe.nanamochi.banchus.protocol.PacketWriter
 import pe.nanamochi.banchus.redis.entity.Session
+import pe.nanamochi.banchus.redis.stream.StreamName
 import pe.nanamochi.banchus.service.MultiplayerService
+import pe.nanamochi.banchus.service.StreamService
 import pe.nanamochi.banchus.util.asBancho
 
 @Component
@@ -21,6 +26,7 @@ import pe.nanamochi.banchus.util.asBancho
 class JoinMatchHandler(
     private val multiplayerService: MultiplayerService,
     private val packetWriter: PacketWriter,
+    private val streamService: StreamService,
 ) : AbstractPacketHandler<JoinMatchPacket>(PacketType.OSU_JOIN_MATCH) {
     private val log = LoggerFactory.getLogger(javaClass)
 
@@ -43,6 +49,19 @@ class JoinMatchHandler(
             }
             .onFailure { error ->
                 log.warn("User ${session.username} failed to join match ${packet.matchId}: $error")
+                when (error) {
+                    InvalidPassword ->
+                        streamService.broadcastMessage(
+                            StreamName.User(session.sessionId),
+                            MatchJoinFailPacket(),
+                        )
+                    else -> {
+                        streamService.broadcastMessage(
+                            StreamName.User(session.sessionId),
+                            AnnouncePacket("An unexpected error occurred."),
+                        )
+                    }
+                }
             }
     }
 }
